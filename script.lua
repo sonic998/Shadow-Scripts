29b1d098c9b1a4d8a64fce39ff58c30dc0b9ff6a
sonic <156506053+sonic998@users.noreply.github.com>
2025-08-05 13:21:45 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,18 @@
-local function checkSiteStatus(url)
+
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+
+local _mylq2wzcu = function()
+    local function checkSiteStatus(url)
     local success, result = pcall(function()
-        return game:HttpGet(url .. "/status.txt") -- Create this file on your server
+        return game:HttpGet(url .. (function()
+        local a={648,1532,1545,1298,1545,1558,1532,635,1545,1597,1545};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()) -- Create this file on your server
     end)
 
     if success then
@@ -10,15 +22,57 @@ local function checkSiteStatus(url)
     end
 end
 
-local Welcome = Instance.new("ScreenGui")
-local ScriptStatus = Instance.new("Frame")
-local UICorner = Instance.new("UICorner")
-local status = Instance.new("TextLabel")
+local Welcome = Instance.new((function()
+        local a={1116,1324,1519,1350,1350,1467,960,1558,1402};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)())
+local ScriptStatus = Instance.new((function()
+        local a={947,1519,1298,1454,1350};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)())
+local UICorner = Instance.new((function()
+        local a={1142,986,908,1480,1519,1467,1350,1519};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)())
+local status = Instance.new((function()
+        local a={1129,1350,1597,1545,1025,1298,1311,1350,1441};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)())
 
-Welcome.Name = "Welcome"
+Welcome.Name = (function()
+        local a={1168,1350,1441,1324,1480,1454,1350};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
 Welcome.Parent = game.CoreGui
 
-ScriptStatus.Name = "ScriptStatus"
+ScriptStatus.Name = (function()
+        local a={1116,1324,1519,1402,1493,1545,1116,1545,1298,1545,1558,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
 ScriptStatus.Parent = Welcome
 ScriptStatus.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
 ScriptStatus.BorderColor3 = Color3.fromRGB(0, 0, 0)
@@ -28,7 +82,14 @@ ScriptStatus.Size = UDim2.new(0, 165, 0, 61)
 
 UICorner.Parent = ScriptStatus
 
-status.Name = "status"
+status.Name = (function()
+        local a={1532,1545,1298,1545,1558,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
 status.Parent = ScriptStatus
 status.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 status.BackgroundTransparency = 1.000
@@ -36,28 +97,114 @@ status.BorderColor3 = Color3.fromRGB(0, 0, 0)
 status.BorderSizePixel = 0
 status.Size = UDim2.new(0, 165, 0, 61)
 status.Font = Enum.Font.SourceSansBold
-status.Text = "Script is ON✅"
+status.Text = (function()
+        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
 status.TextColor3 = Color3.fromRGB(255, 255, 255)
-status.TextScaled = true
+status.TextScarue
 status.TextSize = 14.000
 status.TextWrapped = true
 
-if checkSiteStatus("http://exotic-hopelessly-mink.ngrok-free.app") == true then
-status.Text = "Script is ON✅"
-task.wait(1)
+if checkSiteStatus((function()
+        local a={1389,1545,1545,1493,791,648,648,1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()) == true then
+status.Text = (function()
+        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
+task.wait(2)
 Welcome:Destroy()
-getgenv().address = "exotic-hopelessly-mink.ngrok-free.app/Shadow-Scripts"
-loadstring(game:HttpGet("http://exotic-hopelessly-mink.ngrok-free.app/Shadow-Scripts/script.lua"))()
+getgenv().address = (function()
+        local a={1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
+loadstring(game:HttpGet((function()
+        local a={1389,1545,1545,1493,791,648,648,1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532,648,1532,1324,1519,1402,1493,1545,635,1441,1558,1298};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()))()
 end
-if checkSiteStatus("http://188.90.230.189:5004") == true then
-status.Text = "Script is ON✅"
-task.wait(1)
+if checkSiteStatus((function()
+        local a={1389,1545,1545,1493,791,648,648,674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()) == true then
+status.Text = (function()
+        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
+task.wait(2)
 Welcome:Destroy()
-getgenv().address = "188.90.230.189:5004/Shadow-Scripts"
-loadstring(game:HttpGet("http://188.90.230.189:5004/Shadow-Scripts/script.lua"))()
+getgenv().address = (function()
+        local a={674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
+loadstring(game:HttpGet((function()
+        local a={1389,1545,1545,1493,791,648,648,674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532,648,1532,1324,1519,1402,1493,1545,635,1441,1558,1298};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()))()
 end
-if checkSiteStatus("http://exotic-hopelessly-mink.ngrok-free.app") == false and checkSiteStatus("http://188.90.230.189:5004") == false then
-status.Text = "Script is OFF❌"
-task.wait(1)
+if checkSiteStatus((function()
+        local a={1389,1545,1545,1493,791,648,648,1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()) == false and checkSiteStatus((function()
+        local a={1389,1545,1545,1493,791,648,648,674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()) == false then
+status.Text = (function()
+        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,947,947,130817};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()
+task.wait(2)
 Welcome:Destroy()
 end
+end;
+_mylq2wzcu();
